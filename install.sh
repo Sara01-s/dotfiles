82421e344020be5d0cc7c7b9f97f0d30ec49783e
Sara01-s <saraosses01@gmail.com>
2024-08-03 21:35:29 -0400

    feat (install): added hyprpanel

--- install.sh
+++ install.sh
@@ -87,4 +87,57 @@ install eza 		"eza is a replacement for ls, primarily used for showing icons."
 install bat			"bat is a replacement for cat, it shows files in a more organized way."
 install firefox		"firefox is a web browser."
 install thunar		"thunar is a GUI file explorer."
-install viu			"viu is used to view images and gif inside the terminal"
+install viu			"viu is used to view images and gif inside the terminal."
+install neofetch	"neofetch is used to view system specifications in the terminal."
+install wofi		"wofi is a wayland application runner, see hyprland.conf to check it's keybind."
+
+echo ""
+
+# Hyprpanel
+big_log "Installing HyprPanel..."
+
+echo ""
+sleep 1
+
+cd $HOME/dotfiles/reps/HyprPanel
+
+# Hyprpanel dependencies
+log "Installing HyprPanel dependencies..."
+
+if [ -L "/usr/local/bin/bun" ] && [ -e "/usr/local/bin/bun" ]; then
+    log "bun already installed."
+else
+	curl -fsSL https://bun.sh/install | bash && sudo ln -s $HOME/.bun/bin/bun /usr/local/bin/bun
+fi
+
+install pipewire				"provides a multimedia server for handling audio and video."
+install bluez					"implements the Bluetooth protocol for devices on Linux"
+install bluez-utils				"offers additional tools for managing Bluetooth devices"
+install btop					"displays an interactive system monitor with information about processes, CPU usage, memory, etc."
+install networkmanager			"manages network connections, simplifying network configuration on Linux."
+install dart-sass				"compiles Sass files to CSS for styling web applications."
+install brightnessctl			"allows adjusting screen brightness from the terminal."
+install python 					"interprets and executes scripts written in the Python programming language."
+install gnome-bluetooth-3.0		"provides tools and graphical interfaces for managing Bluetooth devices in GNOME environments."
+install grimblast				"captures screenshots on Wayland-based systems"
+install gpu-screen-recorder  	"records the screen using GPU acceleration for improved performance."
+install hyprpicker				"a color picker tool for Wayland"
+install aylurs-gtk-shell-git 	"provides GTK shell extensions and tools for customization"
+
+log "Starting HyprPanel..."
+if ps aux | grep -v grep | grep -q "ags"; then
+    log "AGS is already running."
+else
+    ags
+fi
+
+log "Setting up dotfiles symlinks..."
+cd $HOME/dotfiles
+stow .
+
+echo ""
+big_log "System installation completed."
+
+neofetch
+
+
